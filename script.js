96dc88c175847a966ad0810bcfbb846a864845c4
xin <chjgreen@qq.com>
2019-01-23 12:49:27 +0800

    存储上次离开时选择的笔记& 保存多会话笔记

--- script.js
+++ script.js
@@ -2,18 +2,28 @@ new Vue({
     el: "#notebook",
     data: {
         content: localStorage.getItem('content') || 'You can write in **markdown**',
-        notes: [],
+        notes: JSON.parse(localStorage.getItem('notes')) || [],
+        selectedId: localStorage.getItem('selected-id') || null,
     },
     computed: {
-      // Markdown 渲染为HTML
-      notePreview () {
-          return marked(this.content)
-      }
+        selectedNote () {
+            // We return the matching note with selectedId
+            return this.notes.find(note => note.id === this.selectedId)
+        },
+
+        notePreview () {
+            // Markdown rendered to HTML
+            return this.selectedNote ? marked(this.selectedNote.content) : ''
+        },
     },
     methods: {
         saveNote() {
             localStorage.setItem('content', this.content);
             this.reportOperation( 'saving')
+        },
+        saveNotes() {
+            localStorage.setItem('notes', JSON.stringify(this.notes))
+            console.log("Notes saved!", new Date())
         },
         reportOperation(opName) {
             console.log('The', opName, 'operation was completed!')
@@ -28,11 +38,21 @@ new Vue({
                 favorite: false,
                 };
             this.notes.push(note)
+        },
+        selectNote(note){
+            this.selectedId = note.id
         }
     },
 
     watch: {
-        content: 'saveNote'
+        content: 'saveNote',
+        notes: {
+            handler: 'saveNotes',
+            deep:true
+        },
+        selectedId(val) {
+          localStorage.setItem('selected-id', val)
+        },
     },
 });
 

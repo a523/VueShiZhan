8732a417a18c99ae6962e19cd3212071d31453ab
xin <chjgreen@qq.com>
2019-01-30 15:16:15 +0800

    重构FAQ

--- support-center/src/components/FAQ.vue
+++ support-center/src/components/FAQ.vue
@@ -1,8 +1,8 @@
 <template>
     <main class="faq">
       <h1>Frenquently Asked Questions</h1>
-      <div class="error" v-if="error">Can't load the questions</div>
-      <Loading v-if="loading"/>
+      <div class="error" v-if="hasRemoteErrors">Can't load the questions</div>
+      <Loading v-if="remoteDataBusy"/>
       <section class="list">
         <article v-for="question of questionLists">
           <h2 v-html="question.title"></h2>
@@ -20,22 +20,6 @@
         mixins: [
           RemoteData({questionLists: 'questions'}),
         ],
-        data () {
-          return {
-            questions: [],
-            error: null,
-            loading: false,
-          }
-        },
-      async created() {
-          this.loading = true
-          try {
-              this.questions = await this.$fetch('questions')
-          } catch (e) {
-              this.error = e
-          }
-          this.loading = false
-        },
     }
 </script>
 
--- support-center/src/mixins/RemoteData.js
+++ support-center/src/mixins/RemoteData.js
@@ -4,26 +4,40 @@ export default function (resources) {
       let initData = {
         remoteDataLoading: 0,
       }
-
+      initData.remoteErrors = {}
       for (const key in resources) {
         initData[key] = null
+        initData.remoteErrors[key] =null
       }
 
       return initData
     },
     methods: {
       async fetchResource (key, url) {
+        this.$data.remoteDataLoading++
         try {
           this.$data[key] = await this.$fetch(url);
         } catch (e) {
           console.log(e)
+          this.$data.remoteErrors[key] = e
         }
+        this.$data.remoteDataLoading--
       }
     },
     created () {
       for (const key in resources) {
         let url = resources[key]
         this.fetchResource(key, url)
+      }
+    },
+    computed:{
+      remoteDataBusy(){
+        return this.$data.remoteDataLoading !== 0
+      },
+      hasRemoteErrors (){
+        return Object.keys(this.$data.remoteErrors).some(
+          key => this.$data.remoteErrors[key]
+        )
       }
     }
   }
